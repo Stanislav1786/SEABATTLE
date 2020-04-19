31456e30b357b290fb41abf8cf5506c0847084a2
Stanislav1786 <ogonek1786@gmail.com>
2020-04-19 13:11:02 +0300

    Нарисовал игровое поле

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
+const CELL_SIZE = 23
+
+
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d') //запрашивает контекст отрисовки
 
 canvas.width = 500
 canvas.height = 500
 
+drawGrid() //используя const  понять как разрисован наш canvas
 
-drawRect({
-    x: 10,
-    y: 10,
-    widht: 150,
-    hegiht: 300,
 
-    strokeStyle: "red",
-    stroke: true,
+// drawRect({
+//     x: 10,
+//     y: 10,
+//     widht: 150,
+//     hegiht: 300,
 
-    fillStyle: "green",
-    fill: true,
+//     strokeStyle: "red",
+//     stroke: true,
 
-    lineWidth: 2
-})
+//     fillStyle: "green",
+//     fill: true,
+
+//     lineWidth: 2
+// })
 
 function drawRect(param) {
     if (!param.fill && !param.stroke) {
@@ -41,4 +46,33 @@ function drawRect(param) {
         context.lineWidth = param.lineWidth
         context.stroke()
     }
+}
+
+function drawGrid() {
+    context.strokeStyle = 'blue'
+    context.lineWidth = 0.5
+
+
+    for (let i = 0; i < canvas.width / CELL_SIZE; i++) {
+        context.beginPath()
+        context.moveTo(i * CELL_SIZE, 0)
+        context.lineTo(i * CELL_SIZE, canvas.height)
+        context.stroke()
+    }
+
+    for (let i = 0; i < canvas.height / CELL_SIZE; i++) {
+        context.beginPath()
+        context.moveTo(0, i * CELL_SIZE)
+        context.lineTo(canvas.width, i * CELL_SIZE)
+        context.stroke()
+    }
+
+    context.lineWidth = 2 //создание красной линии поля
+    context.strokeStyle = 'red'
+
+    context.beginPath()
+    context.moveTo(0, 75)
+    context.lineTo(canvas.width, 75)
+    context.stroke()
+
 }
